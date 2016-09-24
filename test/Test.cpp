3a8c65f85caff41b10c7b882f57c03f600b7c081
rurenko <grurenko@gmail.com>
2016-09-25 02:10:18 +0300

    BinarySearch test

--- test/Test.cpp
+++ test/Test.cpp
@@ -1,5 +1,6 @@
 #include "MergeSort.h"
 #include "QuickSort.h"
+#include "BinarySearch.h"
 #include "gtest/gtest.h"
 
 #include <vector>
@@ -16,4 +17,16 @@ TEST (QuickSort, QuickSort)
     std::vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
     QuickSort(arr.data(), arr.size());
     ASSERT_EQ(std::vector<int>({3, 9, 10, 27, 38, 43, 82}), arr);
+}
+
+TEST (BinarySearch, BinarySearch)
+{ 
+    std::vector<int> arr = {3, 9, 10, 27, 38, 43, 82};
+    
+    ASSERT_EQ(0, BinarySearch(arr.data(), arr.size(), 3));
+    ASSERT_EQ(2, BinarySearch(arr.data(), arr.size(), 10));
+    ASSERT_EQ(5, BinarySearch(arr.data(), arr.size(), 43));
+    ASSERT_EQ(6, BinarySearch(arr.data(), arr.size(), 82));
+    ASSERT_EQ(-1, BinarySearch(arr.data(), arr.size(), 72));
+    ASSERT_EQ(-1, BinarySearch(arr.data(), arr.size(), 0));
 }
